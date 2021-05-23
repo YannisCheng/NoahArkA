ba1dfa2acaebdac6e6d7f99ca1629e460e1322b0
YannisCheng <cwj1714@163.com>
2021-05-23 14:50:45 +0800

    Initial commit

---
+++ ScriptLib/src/main/java/com/cwj/scriptlib/ScriptLibApplication.java
@@ -0,0 +1,13 @@
+package com.cwj.scriptlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptLibApplication.class, args);
+    }
+
+}
