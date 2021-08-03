de1ad540551b0f52a89fa76c595e274fe7aad5b9
YannisCheng <cwj1714@163.com>
2021-08-03 20:32:45 +0800

    NoahArk项目中AppRun（主App）引用Common（Module）和ModuleSpringData（Module）这2个Module中的application.yml文件，最终将2个Module中的application.yml文件中的内容合并到AppRun中的yml文件中。 至此实现了代码合并、配置合并

--- AppRun/src/main/java/com/cwj/apprun/AppRunApplication.java
+++ AppRun/src/main/java/com/cwj/apprun/AppRunApplication.java
@@ -1,12 +1,16 @@
 package com.cwj.apprun;
 
+import com.cwj.common.CommonApplication;
+import com.cwj.datasource.DataSourceApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@Import(value = {CommonApplication.class, DataSourceApplication.class})
 public class AppRunApplication {
 
     public static void main(String[] args) {
--- ModuleSpringData/src/main/java/com/cwj/datasource/DataSourceApplication.java
+++ ModuleSpringData/src/main/java/com/cwj/datasource/DataSourceApplication.java
@@ -1,17 +1,15 @@
 package com.cwj.datasource;
 
-import com.cwj.common.CommonApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 // SpringBoot启动入口
 @SpringBootApplication
 // 为Mybatis设置Mapping的扫描范围
 @MapperScan(basePackages = "com.cwj.datasource.mysql.dao")
 // 添加 Common Module 的容器入口依赖
-@Import(value = {CommonApplication.class})
+//@Import(value = {CommonApplication.class})
 public class DataSourceApplication {
 
     public static void main(String[] args) {
